61d222ce43f2908f5c25989469f837e0d84d2968
Aquilabot <ariesjean2008@hotmail.com>
2021-06-13 10:38:47 -0500

    Amplify todo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'package:xoris_flutter/Values/ResponsiveApp.dart';
 
 import 'Pages/HomePage.dart';
 
+// Amplify Flutter Packages
+import 'package:amplify_flutter/amplify.dart';
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+// Generated in previous step
+import 'amplifyconfiguration.dart';
+
 void main() {
   runApp(MyApp());
 }
